fda147679ada621a6fdf5d70dc7651d191eaaad4
Peter Naylor <peter@dailydriver.gehirn.net>
2025-10-06 06:29:25 -0400

    added strategies abstraction and designed invictus

--- src/include/pnt.h
+++ src/include/pnt.h
@@ -10,10 +10,32 @@
  * networks.
  */
 struct std_conn {
+	socket_t		sock;
 	struct sockaddr_in	address;
-	//TODO add more state
+	enum {
+		INVICTUS,
+		SEVERAIN
+	} conn_strt;
 };
 
-extern struct errep *pnt_traverse_nat(struct sockaddr_in *address, struct std_conn **results);
+/**
+ * Traverses a NATted network using the invictus strategy.
+ *
+ * The invictus strategy exploits the fact that many consumer-grade NAT devices
+ * will attempt to create a NAT mapping with the 'outer port' (the port on the
+ * NAT device open to the network, from which traffic will flow and be
+ * received) having the same number as the 'inner port' (the port from which
+ * the traffic originally flowed, located on a host within the NATted subnet).
+ * This allows the strategy, when employed by a peer, to predict the NAT
+ * mapping that will be present on both its own *and* its peer's NAT devices.
+ * Thus, this function attempts to establish communication between the peers
+ * via this predicted mapping.
+ * @param address the IPv4 address for the NAT device of the target host
+ * @param results a double-pointer buffer to which the results of the operation
+ * will be passed.
+ * @return a struct errep pointer to be used for debugging
+ */
+extern struct errep *pnt_traverse_invictus(struct in_addr *address, struct std_conn **results);
+extern struct errep *pnt_traverse_severain(struct sockaddr_in *address, struct std_conn **results);
 
 #endif //__PNT_H___
--- src/main/nt.c
+++ src/main/nt.c
@@ -9,10 +9,63 @@
 
 #define PNT_BINDPORT 	8668
 
-struct errep *pnt_traverse_nat(struct sockaddr_in *dest, struct std_conn **res)
+struct errep *pnt_traverse_invictus(struct in_addr *address, struct std_conn **res)
 {
 	struct errep *err;
-	char *fnname = "pnt_traverse_nat()";
+	char *fnname = "pnt_traverse_invictus()";
+	struct std_conn *conn;
+	socket_t sock;
+	struct sockaddr_in *dest, *tobind;
+	char *msg = "PLEASE STOP LYING", buf[strlen(msg)];
+	bool received;
+	socklen_t siz = sizeof(struct sockaddr);
+	
+	if ((conn = malloc(sizeof(struct std_conn))) == NULL) {
+		ERREP(err, fnname, "could not allocate memory to standard connection structure");
+		return err;
+	}
+	if ((dest = malloc(sizeof(struct sockaddr_in))) == NULL) {
+		ERREP(err, fnname, "could not allocate memory to internet sockaddr structure");
+		return err;
+	}
+	if ((tobind = malloc(sizeof(struct sockaddr_in))) == NULL) {
+		ERREP(err, fnname, "could not allocate memory to internet sockaddr structure");
+		return err;
+	}
+	dest -> sin_family = AF_INET;
+	dest -> sin_port = PNT_BINDPORT;
+	dest -> sin_addr = *address;
+	memset(tobind, 0, sizeof(struct sockaddr_in));
+	tobind -> sin_family = AF_INET;
+	tobind -> sin_port = PNT_BINDPORT;
+	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		ERREP(err, fnname, "socket for nat traversal could not be created");
+		return err;
+	}
+	if (bind(sock, (struct sockaddr *) &us, sizeof(struct sockaddr)) == -1) {
+		ERREP(err, fnname, "error binding our socket to the designated bindport");
+		return err;
+	}
+	if (sendto(sock, msg, strlen(msg), 0, (struct sockaddr *) dest, siz) < msglen) {
+		ERREP(err, fnname, "error sending a message to our peer");
+		return err;
+	}
+        if (recvfrom(sock, buf, sizeof(buf), MSG_DONTWAIT, (struct sockaddr *) &reply, &siz) < sizeof(buf)) {
+		ERREP(err, fnname, "error recovering message from peer");
+		return err;
+	}
+	conn -> sock = sock;
+	conn -> address = dest;
+	conn -> conn_strt = INVICTUS;
+	*res = conn;
+	ERREP(err, fnname, NULL);
+	return err;
+}
+
+struct errep *pnt_traverse_severain(struct sockaddr_in *dest, struct std_conn **res)
+{
+	struct errep *err;
+	char *fnname = "pnt_traverse_severain()";
 	struct std_conn *link;
 	int sock;
 	struct sockaddr_in us, reply;
